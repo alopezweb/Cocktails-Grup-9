26413f3d59fc62e1d001157156f42858639c35bd
Alberto López <alopezrweb@gmail.com>
2024-11-22 22:03:05 +0100

    removing prop test

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import Header from './components/Header.vue'
 import Banner from './components/Banner.vue';
 import Nav from './components/Nav.vue'
 import Footer from './components/Footer.vue'
-// import HeaderPropTest from './components/HeaderPropTest.vue';
 
 </script>
 
 <template>
 <body>
-    <!-- <HeaderPropTest/> -->
     <Header />
     <Banner/>
     <Nav />
